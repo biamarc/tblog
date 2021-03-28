c872075f2b7cdd086cf1508db563d9240a555a03
Marco B <marco.biagiotti@tilever.com>
2021-03-28 20:38:15 +0200

    Init travels page

---
+++ client/src/pages/MyTravels.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page padding>
+    <h1>My Travels</h1>
+      <p>{{ token }}</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'MyTravels',
+  data () {
+    return {
+      token: null
+    }
+  },
+  created() {
+    this.getJWTToken();
+  },
+  methods: {
+    getJWTToken() {
+      this.$auth.getIdTokenClaims()
+        .then((val) => this.token = val.__raw)
+    }
+  }
+
+}
+</script>

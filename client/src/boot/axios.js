caa0fd6e6033295cd8c77d98b6d116a84e0f5e05
Marco B <marco.biagiotti@tilever.com>
2021-03-30 20:36:21 +0200

    Build client with custom baseURL for endpoint and authHeade

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -1,16 +1,58 @@
 import axios from 'axios'
 import Domain  from '../models/domain'
-function authHeader(config, idToken){
-  config.headers['Authorization'] = `Bearer ${idToken}`
+
+/**
+ * Build Authorization Header
+ * @param config
+ * @param idToken
+ * @return {function(*): *}
+ */
+
+class CustomAxios {
+  // class to build client
+  _axios;
+  // base url to call
+ _baseUrl;
+
+  constructor(axios, baseUrl) {
+    this._axios = axios
+    this._baseUrl = baseUrl
+  }
+
+  authHeader(idToken) {
+    return (config) => {
+      config.headers['Authorization'] = `Bearer ${idToken}`
+      return config;
+    };
+  }
+
+  /**
+   * Build an axios client
+   * @param config configuration
+   * @return an instance of axios client configured
+   */
+  build(config = {baseURL: this._baseUrl , timeout: 5000}) {
+    return this._axios.create(config);
+  }
+
+  /**
+   *
+   * @param config
+   * @param token
+   * @return {*}
+   */
+  buildAuth(token, config = {baseURL: this._baseUrl , timeout: 5000}) {
+    console.log(config)
+    const instance = this.build(config)
+    instance.interceptors.request.use(this.authHeader(token))
+    return instance;
+  }
+
 }
 
 // "async" is optional;
 // more info on params: https://quasar.dev/quasar-cli/boot-files
 export default async ({  Vue  }) => {
-  Vue.prototype.$axios = axios.create({
-    baseURL: Domain.endpoint,
-    timeout: 5000
-  });
+  Vue.prototype.$axios = new CustomAxios(axios,Domain.endpoint);
 }
 
-export {authHeader}

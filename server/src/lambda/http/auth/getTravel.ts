e4ad94355c5ebc333003c103a0f43318c275c8da
Marco B <marco.biagiotti@tilever.com>
2021-04-16 11:10:53 +0200

    Insert new handler to retrieve a single travel

---
+++ server/src/lambda/http/auth/getTravel.ts
@@ -0,0 +1,43 @@
+import 'source-map-support/register'
+
+import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda'
+
+import {getUserId} from "../../utils";
+import {TravelItem} from "../../../models/TravelItem";
+import {getTravel} from "../../../services/TravelsService";
+import {createLogger} from "../../../utils/logger";
+
+const logger = createLogger('getTravel')
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Received event: %s', JSON.stringify(event))
+
+    const travelId = event.pathParameters.travelId
+    logger.info('Get travel: travelId=%s', travelId)
+
+    // extract userId from JWT Token
+    const userId: string = getUserId(event)
+
+    const travel: TravelItem = await getTravel(userId, travelId)
+    if (travel === null) {
+        logger.warn('Request travelId=%s may not exists or not owned by the current user=%s', travelId, userId)
+        return {
+            statusCode: 404,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true
+            },
+            body: JSON.stringify({
+                message: `The requested travel was not found`
+            })
+        }
+    }
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body:  JSON.stringify(travel)
+    }
+}
